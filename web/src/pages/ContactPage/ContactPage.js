868d93858e10695838113d1cc79460cd70a964a4
josemasar <josemanuelsarrion@gmail.com>
2021-01-24 22:43:07 +0100

    contact form

---
+++ web/src/pages/ContactPage/ContactPage.js
@@ -0,0 +1,76 @@
+import BlogLayout from 'src/layouts/BlogLayout/BlogLayout'
+import { Form, TextField, TextAreaField, Submit, FieldError, Label, FormError} from '@redwoodjs/forms'
+import { Flash, useFlash, useMutation } from '@redwoodjs/web'
+import { useForm } from 'react-hook-form'
+
+
+const CREATE_CONTACT = gql`
+  mutation CreateContactMutation($input: CreateContactInput!) {
+    createContact(input: $input) {
+      id
+    }
+  }
+`
+
+
+const ContactPage = () => {
+
+  const formMethods = useForm({ mode: 'onBlur' })
+
+
+  const { addMessage } = useFlash()
+
+  const [create, { loading, error }] = useMutation(CREATE_CONTACT, {
+    onCompleted: () => {
+      formMethods.reset()
+      addMessage('Thank you for your submission!', {
+        style: { backgroundColor: 'green', color: 'white', padding: '1rem' }
+      })
+    },
+  })
+
+  const onSubmit = (data) => {
+  create({ variables: { input: data }})
+  console.log(data)
+  }
+  return (
+    <>
+    <BlogLayout>
+    <Flash timeout={2000} />
+    <Form onSubmit={onSubmit} error={error} formMethods={formMethods} >
+      <FormError
+        error={error}
+        wrapperStyle={{ color: 'red', backgroundColor: 'lavenderblush' }}
+      />
+      <Label name="name" errorClassName="error">
+          Name
+        </Label>
+      <TextField name="name" validation={{ required: true }} errorClassName="error" />
+      <FieldError name="name" className="error" />
+
+      <Label name="email" errorClassName="error">
+          email
+        </Label>
+        <TextField
+          name="email"
+          validation={{
+            required: true,
+          }}
+          errorClassName="error"
+        />
+
+
+      <Label name="message" errorClassName="error">
+          message
+        </Label>
+      <TextAreaField name="message" validation={{ required: true }} errorClassName="error" />
+      <FieldError name="message" className="error" />
+
+      <Submit disabled={loading}>Save</Submit>
+      </Form>
+    </BlogLayout>
+    </>
+  )
+}
+
+export default ContactPage
